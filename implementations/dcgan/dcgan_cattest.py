d9a57339d6c18935474d6004e859b5f883a19ce8
DerienFe <55786232+DerienFe@users.noreply.github.com>
2021-02-21 20:26:15 +0000

    Update dcgan_cattest.py

--- implementations/dcgan/dcgan_cattest.py
+++ implementations/dcgan/dcgan_cattest.py
@@ -19,16 +19,16 @@ from self_dataset import *
 os.makedirs("images", exist_ok=True)
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--n_epochs", type=int, default=400, help="number of epochs of training")
+parser.add_argument("--n_epochs", type=int, default=200, help="number of epochs of training")
 parser.add_argument("--batch_size", type=int, default=64, help="size of the batches")
 parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rate")
 parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
 parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of first order momentum of gradient")
 parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
 parser.add_argument("--latent_dim", type=int, default=100, help="dimensionality of the latent space")
-parser.add_argument("--img_size", type=int, default=128, help="size of each image dimension")
+parser.add_argument("--img_size", type=int, default=64, help="size of each image dimension")
 parser.add_argument("--channels", type=int, default=3, help="number of image channels")
-parser.add_argument("--sample_interval", type=int, default=200, help="interval between image sampling")
+parser.add_argument("--sample_interval", type=int, default=400, help="interval between image sampling")
 opt = parser.parse_args()
 print(opt)
 
@@ -134,7 +134,7 @@ dataloader = torch.utils.data.DataLoader(
 )
 """
 transform = transforms.Compose(
-        [transforms.RandomResizedCrop(opt.img_size),
+        [transforms.RandomResizedCrop(opt.img_size, scale=(0.8, 1.0)),
          transforms.RandomHorizontalFlip(),
          transforms.ToTensor(),
          #transforms.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225]),
